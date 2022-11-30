aa193880d767a14f16b10d0d89d471121f0533d4
Deni Brkan <brkandeni@hotmail.com>
2022-11-30 19:11:33 +0100

    add appointment types in schedule sidebar

--- client/src/app/pages/appointments/appointments.component.ts
+++ client/src/app/pages/appointments/appointments.component.ts
@@ -5,7 +5,9 @@ import { delay, shareReplay } from 'rxjs/operators';
 import { AppointmentEditComponent } from 'src/app/components/appointment/appointment-edit/appointment-edit.component';
 import { Appointment } from 'src/app/models/appointment';
 import { AppointmentParams } from 'src/app/models/appointmentParams';
+import { AppointmentType } from 'src/app/models/appointmentType';
 import { Barber } from 'src/app/models/barber';
+import { AppointmentTypeService } from 'src/app/_services/appointment-type.service';
 import { AppointmentService } from 'src/app/_services/appointment.service';
 import { BarberService } from 'src/app/_services/barber.service';
 
@@ -19,23 +21,30 @@ export class AppointmentsComponent implements OnInit {
   params: AppointmentParams = {} as AppointmentParams;
   barbers$: Observable<Barber[]>;
   appointments$: Observable<Appointment[]>;
+  appointmentTypes$: Observable<AppointmentType[]>;
 
   editAppointmentModal: BsModalRef;
 
   constructor(
     private barberService: BarberService,
     private appointmentService: AppointmentService,
+    private appointmentTypeService: AppointmentTypeService,
     private modalService: BsModalService
   ) {}
 
   ngOnInit(): void {
     this.loadBarbers();
+    this.loadAppointmentTypes();
   }
 
   loadAppointments() {
     this.appointments$ = this.appointmentService
       .get(this.params)
       .pipe(shareReplay(), delay(0));
+  }
+
+  loadAppointmentTypes() {
+    this.appointmentTypes$ = this.appointmentTypeService.getAppointmentTypes();
   }
 
   loadBarbers() {
